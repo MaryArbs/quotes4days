cb14df89f93717aa2e36e09a4c26c02be9593149
MaryArbs <mary.arbachauskas@gmail.com>
2020-07-31 14:40:37 -0400

    setup class component

--- src/App.js
+++ src/App.js
@@ -2,3 +2,12 @@ import React from "react";
 import QuoteAndAuthor from "./QuoteAndAuthor";
 import quotes from './QuotesDatabase'
 import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author,
+    };
+  }
