62b624d11aefc0066f566dbef5424f6e2894030e
MaryArbs <mary.arbachauskas@gmail.com>
2020-08-01 12:45:46 -0400

    update functions

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ class App extends React.Component {
   shuffleQuotes(array) {
     return array.sort(() => Math.random() - 0.5)
   }
+  handleClick = () => {
+    const generateRandomQuote = this.randomQuote();
+    this.setState({
+      quote: generateRandomQuote.quote,
+      author: generateRandomQuote.author
+    });
+    this.shuffleQuotes(quotes)
+  };
+
+  randomColor() {
+    const color = `rgb(
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)},
+      ${Math.floor(Math.random() * 155)})`;
+    return color;
+  }
 
 }
 export default App;
