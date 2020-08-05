8e98387d852b373817f6ededdf9ba0d630bb78c6
MaryArbs <mary.arbachauskas@gmail.com>
2020-08-05 14:58:59 -0400

    update render

--- src/QuoteAndAuthor.js
+++ src/QuoteAndAuthor.js
@@ -2,7 +2,19 @@ import React from "react";
 
 class QuoteAndAuthor extends React.Component {
     render() {
-    }
-}
+        const randomColor = this.props.displayColor();
+        const html = document.documentElement;
+        html.style.backgroundColor = randomColor;
+
+        return (
+            <div style={{ backgroundColor: "white" }} className="quotebox">
+                <div
+                    className="fadeIn"
+                    key={Math.random()}
+                    style={{ color: randomColor }}
+                >
+                    );
+                    }
+                    }
 
 export default QuoteAndAuthor;
