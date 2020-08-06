20f4b488af7ee308fe0dc69ca14e62af9bc65d65
MaryArbs <mary.arbachauskas@gmail.com>
2020-08-06 14:06:11 -0400

    add quotes

--- src/QuotesDatabase.js
+++ src/QuotesDatabase.js
@@ -1,4 +1,14 @@
 const quotes = [
+    {
+        "quote": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "author": "Thomas Edison"
+    }, {
+        "quote": "You can observe a lot just by watching.",
+        "author": "Yogi Berra"
+    }, {
+        "quote": "A house divided against itself cannot stand.",
+        "author": "Abraham Lincoln"
+    },
 
 ]
 export default quotes
