c15c1509449863134f02718208f59a668cdefcb0
MaryArbs <mary.arbachauskas@gmail.com>
2020-08-07 09:11:21 -0400

    add quotes

--- src/QuotesDatabase.js
+++ src/QuotesDatabase.js
@@ -9,6 +9,28 @@ const quotes = [
         "quote": "A house divided against itself cannot stand.",
         "author": "Abraham Lincoln"
     },
+    {
+        "quote": "Difficulties increase the nearer we get to the goal.",
+        "author": "Johann Wolfgang von Goethe"
+    }, {
+        "quote": "Fate is in your hands and no one elses",
+        "author": "Byron Pulsifer"
+    }, {
+        "quote": "Be the chief but never the lord.",
+        "author": "Lao Tzu"
+    }, {
+        "quote": "Nothing happens unless first we dream.",
+        "author": "Carl Sandburg"
+    }, {
+        "quote": "Well begun is half done.",
+        "author": "Aristotle"
+    }, {
+        "quote": "Life is a learning experience, only if you learn.",
+        "author": "Yogi Berra"
+    }, {
+        "quote": "Self-complacency is fatal to progress.",
+        "author": "Margaret Sangster"
+    },
 
 ]
 export default quotes
